ca43a0c40ec2c7307afbbbb27a1f808f8583f1e5
Michael Reynolds <reynoldsm88@gmail.com>
2018-07-16 17:16:24 -0400

    forgot to actually calculate pi

--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
@@ -6,7 +6,7 @@ import org.scalatra.{Ok, ScalatraServlet}
 class SparkPiServlet extends ScalatraServlet {
     get( "/sparkpi" ) {
         val spark = new SparkContext( new SparkConf().setAppName("Radanalytics IO Scakatra Tutorial") )
-        val sparkPi = new SparkPI( spark,2 )
+        val sparkPi = new SparkPI( spark,2 ).calculate()
         Ok( "Pi is roughly $sparkPi" )
     }
 }
