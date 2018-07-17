fba11f8b705eb77ef75bcedf862e6d80b10821c0
Michael Reynolds <reynoldsm88@gmail.com>
2018-07-17 10:51:48 -0400

    adding the ability to handle the 'scale' parameter and general cleanup

--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPI.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPI.scala
@@ -1,13 +1,10 @@
 package io.radanalytics.examples.scalatra.sparkpi
 
 import org.apache.spark.SparkContext
-import java.math.BigDecimal
+
 import scala.math.random
 
 class SparkPI( spark : SparkContext, scale : Int ) {
-
-    val applicationName = "Spark PI Scalatra Tutorial"
-
     def calculate( ) : Double = {
         val n = math.min( 100000L * scale, Int.MaxValue ).toInt // avoid overflow
         val count = spark.parallelize( 1 until n, scale ).map( i => {
@@ -17,5 +14,4 @@ class SparkPI( spark : SparkContext, scale : Int ) {
         } ).reduce( _ + _ )
         4.0 * count / ( n - 1 )
     }
-
 }
--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
@@ -5,9 +5,10 @@ import org.scalatra.{Ok, ScalatraServlet}
 
 class SparkPiServlet extends ScalatraServlet {
     get( "/sparkpi" ) {
-        val spark = new SparkContext( new SparkConf().setAppName("Radanalytics IO Scakatra Tutorial") )
-        val sparkPi = new SparkPI( spark,2 ).calculate()
+        val scale : Int = if( params("scale") != null ) Integer.parseInt(params("scale") ) else 2
+        val spark = new SparkContext( new SparkConf().setAppName( "Radanalytics IO SparkPI Scalatra Tutorial" ) )
+        val sparkPi = new SparkPI( spark, scale ).calculate()
         spark.stop()
-        Ok( "Pi is roughly $sparkPi" )
+        Ok( "Pi is roughly " + sparkPi )
     }
 }
