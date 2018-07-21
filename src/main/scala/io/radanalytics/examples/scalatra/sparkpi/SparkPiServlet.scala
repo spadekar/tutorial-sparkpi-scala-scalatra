ed67046c2e52b52f1582bb9d5c584abf08303aec
Michael Reynolds <reynoldsm88@gmail.com>
2018-07-20 20:03:47 -0400

    fixing an issue when providing an empty scale parameter

--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
@@ -5,10 +5,12 @@ import org.scalatra.{Ok, ScalatraServlet}
 
 class SparkPiServlet extends ScalatraServlet {
     get( "/sparkpi" ) {
-        val scale : Int = if( params("scale") != null ) Integer.parseInt(params("scale") ) else 2
+        val scale = Integer.parseInt( params.getOrElse( "scale", "2" ) )
+
         val spark = new SparkContext( new SparkConf().setAppName( "Radanalytics IO SparkPI Scalatra Tutorial" ) )
         val sparkPi = new SparkPI( spark, scale ).calculate()
         spark.stop()
+
         Ok( "Pi is roughly " + sparkPi )
     }
 }
