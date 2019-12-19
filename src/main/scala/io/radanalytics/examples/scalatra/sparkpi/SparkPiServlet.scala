c2c5f68b85e713b229744343bad2e89eadbf1a14
Shailesh Padekar <shailesh.padekar@gmail.com>
2019-12-19 23:19:48 +0530

    error missing bracket

--- src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
+++ src/main/scala/io/radanalytics/examples/scalatra/sparkpi/SparkPiServlet.scala
@@ -6,32 +6,33 @@ import org.scalatra.{Ok, ScalatraServlet}
 
 class SparkPiServlet extends ScalatraServlet {
 
-    get("/"){
-        Ok( "Scala Scalatra SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app." )
+    get("/") {
+        Ok("Scala Scalatra SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app.")
     }
 
-    get( "/sparkpi" ) {
-        val scale = Integer.parseInt( params.getOrElse( "scale", "2" ) )
+    get("/sparkpi") {
+        val scale = Integer.parseInt(params.getOrElse("scale", "2"))
 
-        val spark = new SparkContext( new SparkConf().setAppName( "Radanalytics IO SparkPI Scalatra Tutorial" ) )
-        val sparkPi = new SparkPI( spark, scale ).calculate()
+        val spark = new SparkContext(new SparkConf().setAppName("Radanalytics IO SparkPI Scalatra Tutorial"))
+        val sparkPi = new SparkPI(spark, scale).calculate()
         spark.stop()
 
-        Ok( "Pi is roughly " + sparkPi )
+        Ok("Pi is roughly " + sparkPi)
     }
 
-    get( "/featureProcessing" ) {
+    get("/featureProcessing") {
         //val inputFeatures:String = params.getOrElse( "inputfeatures","customerID.gender")
         //val transformations:String = params.getOrElse( "transformations", "test" )
         //val datasetName:String = params.getOrElse( "datasetName", "xyz" )
 
-        val inputFeatures:String = "customerID.gender"
-        val transformations:String = "test"
-        val datasetName:String = "xyz"
+        val inputFeatures: String = "customerID.gender"
+        val transformations: String = "test"
+        val datasetName: String = "xyz"
         val spark = SparkSession.builder.appName("Radanalytics IO SparkPI Scalatra Tutorial").getOrCreate()
         //val sc = new SparkContext( new SparkConf().setAppName( "Radanalytics IO SparkPI Scalatra Tutorial" ) )
-        val featureProcessing = new FeatureProcessing(spark).processFeatures( inputFeatures,transformations,datasetName )
+        val featureProcessing = new FeatureProcessing(spark).processFeatures(inputFeatures, transformations, datasetName)
         spark.stop()
 
-        Ok( "Pi is roughly " + featureProcessing )
+        Ok("Pi is roughly " + featureProcessing)
+    }
 }
